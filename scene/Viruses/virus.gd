995db22879c916d87c55dd53e6eaa00df3581db8
LECLERC Simon <39391603+leclercsimon74@users.noreply.github.com>
2023-09-19 18:04:38 +0300

    Add files via upload
    Update virus

--- scene/Viruses/virus.gd
+++ scene/Viruses/virus.gd
@@ -1,7 +1,7 @@
 extends RigidBody2D
 
+signal death
 signal virus_destroyed(score)
-signal death_screen
 
 @export var speed := 30
 @export var pos_rnd := 50
@@ -9,11 +9,10 @@ signal death_screen
 @export var penalty_time_incell := 10.0
 @export var degradation_time := 3.0
 @export var cell_entry_time := 5.0
-
 @export var score := 10
 
 var rng := RandomNumberGenerator.new()
-var speed_out := rng.randf_range(speed-(speed/3), speed+(speed/3))
+var speed_out := rng.randf_range(speed-(speed/3.0), speed+(speed/3.0))
 var speed_in := speed_out/2
 
 @onready var target := get_parent().get_parent().get_node("cell") #target character node
@@ -21,13 +20,13 @@ var speed_in := speed_out/2
 
 var out_cell := true
 
-
 var target_pos := Vector2.ZERO
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$"1s timer".start(1)
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -40,12 +39,16 @@ func _on_area_2d_body_entered(body):
 	body.set_collision_mask_value(5, false)
 	body.set_collision_layer_value(5, false)
 	#virus
-	self.set_collision_layer_value(4, false)
-	self.set_collision_mask_value(1, false)
-	self.set_collision_mask_value(4, false)
-	$Area2D.set_collision_mask_value(5, false)
-	$Area2D.set_collision_mask_value(6, false)
-	
+	$CollisionShape2D.queue_free()
+#	self.set_collision_layer_value(4, false)
+#	self.set_collision_mask_value(1, false)
+#	self.set_collision_mask_value(2, false)
+#	self.set_collision_mask_value(4, false)
+	$Area2D.queue_free()
+	$Cell_detection.queue_free()
+#	$Area2D.set_collision_mask_value(5, false)
+#	$Area2D.set_collision_mask_value(6, false)
+
 	if out_cell: #quickly destroy virus
 		if ($Cell_atach.time_left > 0):
 			$Cell_atach.stop()
@@ -85,15 +88,15 @@ func _on_area_2d_body_entered(body):
 		
 
 func _on_cell_detection_body_entered(body): #cell!
-	#print(body.name) #for debug
+	print(self.name+" interacts with "+body.name) #for debug
 	if body.name == 'cellinner':
 		print('Virus on the cell!')
 		$Cell_atach.start(cell_entry_time)
 
 	if body.name == 'Nucleus':
 		print('Virus touch the nucleus!')
-		emit_signal("death_screen")
-		
+		emit_signal("death")
+
 
 func _on_s_timer_timeout():
 	target_pos = target.global_position + Vector2(rng.randf_range(-pos_rnd, pos_rnd), rng.randf_range(-pos_rnd, pos_rnd))
@@ -107,10 +110,12 @@ func _on_explosion_timeout():
 
 func _on_cell_atach_timeout():
 		print('Virus in the cell!')
-		out_cell = true
+		out_cell = false
 		self.set_collision_mask_value(1, false) #cell
+		self.set_collision_mask_value(2, false) #cell
 		self.set_collision_mask_value(3, true) #organelle
 		$Area2D.set_collision_mask_value(6, true) #vesicle
+		$Cell_detection.set_collision_mask_value(1, false)
 		$Cell_detection.set_collision_mask_value(2, false)
 		$Cell_detection.set_collision_mask_value(3, true)
 		target = get_parent().get_parent().get_node("cell").get_node('Nucleus') #target character node
